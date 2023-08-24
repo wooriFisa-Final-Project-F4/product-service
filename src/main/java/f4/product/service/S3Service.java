10fc8eb725bb3c43146c8c029f838af9963e264f
Jiwoon <kfromh0136@Naver.com>
2023-08-25 06:59:37 +0900

    [feat] 상품 등록 서비스 로직 구현
    [ProductSaveRequestDto]
    상품을 등록하기 위해 필요한 데이터들
    
    [ProductService]
    `saveProduct`
    상품을 등록하는 메소드
    
    [S3Service]
    MulipartFile을 파라미터로 전달받아 S3에 업로드 하고 url을 반환받음.
    
    [S3ServiceTest]
    S3에 성공적으로 업데이트 되는지 확인하는 코드

---
+++ src/main/java/f4/product/service/S3Service.java
@@ -0,0 +1,39 @@
+package f4.product.service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import f4.product.constant.CustomErrorCode;
+import f4.product.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+  private final AmazonS3Client s3Client;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucketName;
+
+  /* MultipartFile 타입의 파일을 받아 S3에 업로드 하고 해당 파일의 엔드포인트를 반환함*/
+  public String uploadFile(MultipartFile file) {
+    String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+    try {
+      ObjectMetadata metadata = new ObjectMetadata();
+      metadata.setContentLength(file.getSize());
+      s3Client.putObject(
+          new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
+      return s3Client.getUrl(bucketName, fileName).toString();
+    } catch (AmazonServiceException e) {
+      throw new CustomException(CustomErrorCode.UPLOAD_FAIL_RESPONSE, e);
+    } catch (IOException e) {
+      throw new CustomException(CustomErrorCode.EMPTY_UPLOAD_FILE, e);
+    }
+  }
+}
