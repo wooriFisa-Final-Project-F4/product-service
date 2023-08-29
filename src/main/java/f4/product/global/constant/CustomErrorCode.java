f9370aec9502fad1fdae1c07eec577473239836c
Jiwoon <kfromh0136@Naver.com>
2023-08-29 10:49:25 +0900

    [refactor] cutomerror
    path 제거하고 time 추가

--- src/main/java/f4/product/global/constant/CustomErrorCode.java
+++ src/main/java/f4/product/global/constant/CustomErrorCode.java
@@ -8,19 +8,17 @@ import lombok.Getter;
 public enum CustomErrorCode {
 
   // Bad Request 400
-  UPLOAD_FAIL_RESPONSE("/product/v1", 400, "s3 업로드 중 에러가 발생했습니다"),
-  EMPTY_UPLOAD_FILE("/product/v1", 400, "등록할 s3 이미지 파일이 비어있습니다."),
-  INVALID_AUCTION_STATUS("/product/v1/save", 400, "잘못된 옥션 상태입니다."),
-  ALREADY_REGISTER_PRODUCT("/product/v1/save", 400, "이미 등록된 상품입니다."),
-  NOT_FOUND_PRODUCT("/product/v1/{productId}", 400, "존재하지 않는 상품입니다."),
-  NOT_FOUND_ARTIST("/product/v1/by-artist", 400, "존재하지 않는 아티스트입니다.");
+  UPLOAD_FAIL_RESPONSE(400, "s3 업로드 중 에러가 발생했습니다"),
+  EMPTY_UPLOAD_FILE(400, "등록할 s3 이미지 파일이 비어있습니다."),
+  INVALID_AUCTION_STATUS(400, "잘못된 옥션 상태입니다."),
+  ALREADY_REGISTER_PRODUCT(400, "이미 등록된 상품입니다."),
+  NOT_FOUND_PRODUCT(400, "존재하지 않는 상품입니다."),
+  NOT_FOUND_ARTIST(400, "존재하지 않는 아티스트입니다.");
 
   // Unathorized 401
 
   // Forbidden 402
 
-
-  private final String path;
   private final int code;
   private final String message;
 }
