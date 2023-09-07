80c7c3943ef11bcd79ab324899873ae5fd730295
Jiwoon <kfromh0136@Naver.com>
2023-09-07 12:06:03 +0900

    [build] 배포 테스트 준비
    * 프로젝트 이름 변경
    product -> product-service
    
    * dockerfile 작성
    
    * application.properties 깃 이그노어에서 제외하고 config-server import 하도록 설정함.

--- src/main/java/f4/product/ProductServiceApplication.java
+++ src/main/java/f4/product/ProductServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class ProductApplication {
+public class ProductServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ProductApplication.class, args);
+    SpringApplication.run(ProductServiceApplication.class, args);
   }
 
 }
